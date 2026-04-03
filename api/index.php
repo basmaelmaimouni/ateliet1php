33e228199001f8a72653a701c6c0a117e38db5db
basmaelmaimouni <basmaelmaimouni6@gmail.com>
2026-04-03 08:59:54 +0100

    first commit

---
+++ api/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WELCOME TO MY PORTFOLIO</h1>
+
+	<?php
+   // include_once 'traitements.php';
+
+
+
+
+
+	$groupe="dev101";
+	$plt="vercel";
+
+	echo("<h2>premier site de $groupe sur $plt !!!");
+
+
+    echo("<h3><a href='Compte Rendu Atelier 1.pdf'>Atelier 1 : </h3><a/>");
+     
+   // triangle(10);
+
+	
+	
+	?>
+    
+</body>
+</html>
